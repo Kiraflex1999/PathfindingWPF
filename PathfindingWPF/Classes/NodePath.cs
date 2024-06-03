10a572c0dc03aabb1d9eb46946c8c41737594154
Doberstein, Dustin <dustin.doberstein@tsbw.cloud>
2024-06-03 15:09:09 +0200

    + NodePath.cs

---
+++ PathfindingWPF/Classes/NodePath.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Media;
+
+namespace PathfindingWPF.Classes
+{
+    internal class NodePath
+    {
+        public Node StartNode { get; set; }
+        public Node EndNode { get; set; }
+        public PathGeometry PathGeometry { get; set; }
+
+        public NodePath(Node startNode, Node endNode, PathGeometry pathGeometry) 
+        {
+            StartNode = startNode;
+            EndNode = endNode;
+            PathGeometry = pathGeometry;
+        }
+    }
+}

0d442d7a9ef8055be87a3ba3b3884497d23563fa
Doberstein, Dustin <dustin.doberstein@tsbw.cloud>
2024-06-03 10:36:02 +0200

    + Pathfinder.Start()

--- PathfindingWPF/Classes/Node.cs
+++ PathfindingWPF/Classes/Node.cs
@@ -5,23 +5,25 @@ namespace PathfindingWPF.Classes
     internal class Node
     {
         public Point Point { get; set; }
-        public List<Node> Neighbors { get; set; }
+        public List<Node> NeighborNodes { get; set; }
         public double Radius { get; set; } = 10;
 
         public double GCost { get; set; }   //distance from starting node
         public double HCost { get; set; }   //distance from end node
         public double FCost { get; set; }   //GCost + HCost
 
+        public Node? NextHop { get; set; }
+
         public Node(Point point) 
         {
             Point = point;
-            Neighbors = new List<Node>();
+            NeighborNodes = new List<Node>();
         }
 
         public Node(Point point, List<Node> neighbors) 
         {
             Point = point;
-            Neighbors = neighbors;
+            NeighborNodes = neighbors;
         }
     }
 }
---
+++ PathfindingWPF/Classes/PathFinder.cs
@@ -0,0 +1,55 @@
+﻿
+namespace PathfindingWPF.Classes
+{
+    internal class PathFinder
+    {
+        public List<Node>? Start(Node startNode, Node endNode)
+        {
+            HashSet<Node> closedSet = new HashSet<Node>();
+            List<Node> openSet = new List<Node> { startNode };
+
+            while (openSet.Count > 0)
+            {
+                Node currentNode = openSet.OrderBy(node => node.FCost).First();
+
+                if (currentNode == endNode)
+                {
+                    return ReconstructPath(startNode, endNode);
+                }
+
+                openSet.Remove(currentNode);
+                closedSet.Add(currentNode);
+
+                foreach (Node neighborNode in currentNode.NeighborNodes)
+                {
+                    if (!closedSet.Contains(neighborNode))
+                    {
+                        neighborNode.NextHop = currentNode;
+                        neighborNode.GCost = CalculateGCost(currentNode, neighborNode);
+                        neighborNode.HCost = CalculateHCost(neighborNode, endNode);
+                        neighborNode.FCost = neighborNode.GCost + neighborNode.HCost;
+
+                        openSet.Add(neighborNode);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private double CalculateHCost(Node neighborNode, Node endNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double CalculateGCost(Node currentNode, Node neighborNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Node> ReconstructPath(Node startNode, Node endNode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

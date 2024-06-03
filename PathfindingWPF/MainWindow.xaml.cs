9bcf1cd3211329f7983e416967dd25a189b7f35d
Doberstein, Dustin <dustin.doberstein@tsbw.cloud>
2024-06-03 09:10:54 +0200

    + Canvas + DrawPathOnCanvas

--- PathfindingWPF/MainWindow.xaml.cs
+++ PathfindingWPF/MainWindow.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PathfindingWPF
@@ -19,6 +12,38 @@ namespace PathfindingWPF
         public MainWindow()
         {
             InitializeComponent();
+            DrawPathOnCanvas();
+        }
+
+        private void DrawPathOnCanvas()
+        {
+            Path myPath = new Path
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2,
+                Fill = Brushes.LightBlue,
+            };
+
+            GeometryGroup geometryGroup = new GeometryGroup();
+
+            RectangleGeometry rectangleGeometry = new RectangleGeometry(new Rect(50, 50, 150, 100));
+            geometryGroup.Children.Add(rectangleGeometry);
+
+            EllipseGeometry ellipseGeometry = new EllipseGeometry(new Point(300, 300), 50, 50);
+            geometryGroup.Children.Add(ellipseGeometry);
+
+            PathGeometry pathGeometry = new PathGeometry();
+            PathFigure pathFigure = new PathFigure { StartPoint = new Point(300, 300) };
+            pathFigure.Segments.Add(new LineSegment(new Point(400, 300), true));
+            pathFigure.Segments.Add(new LineSegment(new Point(400, 400), true));
+            pathFigure.Segments.Add(new LineSegment(new Point(300, 400), true));
+            pathFigure.Segments.Add(new LineSegment(new Point(300, 300), true));
+            pathGeometry.Figures.Add(pathFigure);
+            geometryGroup.Children.Add(pathGeometry);
+
+            myPath.Data = geometryGroup;
+
+            myCanvas.Children.Add(myPath);
         }
     }
 }
